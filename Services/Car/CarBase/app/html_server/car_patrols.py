479c5146e6f27182ad5d862f6b9615660e3da60f
Dolev Algam <1@2.com>
2019-12-08 11:24:25 +0200

    Added run over in scenario1

--- Services/Car/CarBase/app/html_server/car_patrols.py
+++ Services/Car/CarBase/app/html_server/car_patrols.py
@@ -2,11 +2,22 @@ import novideo_dir as video_dir
 import nocar_dir as car_dir
 import nomotor as motor
 import time
+import requests
 
 def scenario1():
 	motor.forward()
 	time.sleep(5)
 	motor.ctrl(0)
+	r = requests.get('http://jetson/scenario1')
+	if r.text == "attack"
+	motor.setSpeed(200)
+		motor.forward()
+		time.sleep(1)
+		motor.ctrl(0)
+	else
+		motor.backward()
+		time.sleep(5)
+		motor.ctrl(0)
 
 def sectionA():
 	motor.forward()
