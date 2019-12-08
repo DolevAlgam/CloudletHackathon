ff7ac81b8bdcc83353a3881675eef37f74e7ae58
Dolev Algam <1@2.com>
2019-12-08 12:09:27 +0200

    Separte API call to jetson scan to independent function

--- Services/Car/CarBase/app/html_server/car_patrols.py
+++ Services/Car/CarBase/app/html_server/car_patrols.py
@@ -4,17 +4,21 @@ import nomotor as motor
 import time
 import requests
 
+def scanSuspect():
+	r = requests.get('http://jetson/scan')
+        if r.text == "attack":
+                motor.setSpeed(200)
+                motor.forward()
+                time.sleep(1)
+                motor.ctrl(0)
+	return r.text
+
 def scenario1():
 	motor.forward()
 	time.sleep(5)
 	motor.ctrl(0)
-	r = requests.get('http://jetson/scenario1')
-	if r.text == "attack":
-	motor.setSpeed(200)
-		motor.forward()
-		time.sleep(1)
-		motor.ctrl(0)
-	else:
+	scanSuspect()
+	if r.text != "attack":
 		motor.backward()
 		time.sleep(5)
 		motor.ctrl(0)
@@ -25,6 +29,13 @@ def sectionA():
 	car_dir.turn_right()
 	time.sleep(2)
 	motor.ctrl(0)
+	scanSuspect()
+        if r.text != "attack":
+                motor.backward()
+                time.sleep(2)
+		car_dir.turn_right()
+	        time.sleep(3)
+                motor.ctrl(0)
 
 def sectionB():
 	motor.forward()
@@ -32,4 +43,11 @@ def sectionB():
         car_dir.turn_left()
         time.sleep(3)
         motor.ctrl(0)
+	scanSuspect()
+        if r.text != "attack":
+                motor.backward()
+                time.sleep(3)
+                car_dir.turn_right()
+                time.sleep(2)
+                motor.ctrl(0)
 
