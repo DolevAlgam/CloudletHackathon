e194b74ad74c9bb03a63b37b83e8f3db5a390fe5
matancarmeli7 <45543087+matancarmeli7@users.noreply.github.com>
2019-12-10 15:28:01 +0200

    Update alarmSound.py

--- Services/Alarm/senerio1/alarmSound.py
+++ Services/Alarm/senerio1/alarmSound.py
@@ -5,12 +5,10 @@ import flask
 app = flask.Flask(__name__)
 @app.route('/on')
 def on():
-    os.system('mocp -p Battlefield-3-M-COM-alarm-sound.mp3')
+    wave_obj = sa.WaveObject.from_wave_file("Battlefield-3.wav")
+    play_obj = wave_obj.play()
+    time.sleep(10)
+    play_obj.stop()
     return flask.Response(response="on", status=200, mimetype='text/xml')
-
-@app.route('/off')
-def off():
-    os.system('mocp -s')
-    return flask.Response(response="off", status=200, mimetype='text/xml')
 
 app.run(host='0.0.0.0', debug=False)
