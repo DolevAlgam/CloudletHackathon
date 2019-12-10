a496fb5f5d63ec3181664ac882caf8f41ffa8ab3
matancarmeli7 <45543087+matancarmeli7@users.noreply.github.com>
2019-12-10 14:31:51 +0200

    Update alarmSound.py

--- Services/Alarm/senerio1/alarmSound.py
+++ Services/Alarm/senerio1/alarmSound.py
@@ -1,9 +1,14 @@
-from playsound import playsound
+import os
+import time
 from flask import Flask
 
 app = Flask(__name__)
-@app.route('/alarm')
-def alarm():
-    playsound('Battlefield-3-M-COM-alarm-sound.mp3')
+@app.route('/on')
+def on():
+    os.system('mocp -p Battlefield-3-M-COM-alarm-sound.mp3')
+    
+@app.route('/off')
+def off():
+    os.system('mocp -s')
 
 app.run(host='0.0.0.0', debug=False)
