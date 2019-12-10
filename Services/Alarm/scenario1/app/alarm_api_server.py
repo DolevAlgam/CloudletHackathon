ef9a723b16ab4c59abc20b3a54b4238bcf6d7974
matancarmeli7 <45543087+matancarmeli7@users.noreply.github.com>
2019-12-10 22:46:17 +0200

    Update alarm_api_server.py

--- Services/Alarm/scenario1/app/alarm_api_server.py
+++ Services/Alarm/scenario1/app/alarm_api_server.py
@@ -9,7 +9,7 @@ app = flask.Flask(__name__)
 
 @app.route('/on', methods=['GET'])
 def on():
-	r = requests.get(GETURL)
 	return flask.Response(response="on", status=200, mimetype='text/xml')
+	r = requests.get(GETURL)
 
 app.run(host='0.0.0.0', port=5000, debug=False)
