a9b3a029fb0d847c922475b80b56c1fcf3dee8c0
matancarmeli7 <45543087+matancarmeli7@users.noreply.github.com>
2019-12-10 14:44:17 +0200

    Update alarmSound.py

--- Services/Alarm/senerio1/alarmSound.py
+++ Services/Alarm/senerio1/alarmSound.py
@@ -6,9 +6,11 @@ app = Flask(__name__)
 @app.route('/on')
 def on():
     os.system('mocp -p Battlefield-3-M-COM-alarm-sound.mp3')
-    
+    return flask.Response(response="on", status=200, mimetype='text/xml')
+
 @app.route('/off')
 def off():
     os.system('mocp -s')
+    return flask.Response(response="off", status=200, mimetype='text/xml')
 
 app.run(host='0.0.0.0', debug=False)
